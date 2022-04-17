cc1665196fc6bd5a1571e84d104f01f4e6058e91
“MythMega” <unconfigured@null.spigotmc.org>
2022-04-17 17:35:51 +0200

    Automatic Commit

--- mmk8ds/statsviewers/views.py
+++ mmk8ds/statsviewers/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse
+from .models import *
+from django.template import loader
 
 
 def index(request):
-    
+    template = loader.get_template('statsviewers/index.html')
+    data = {}
+    return HttpResponse(template.render(data, request))
