ad1e77eef3fb20571254f8c3ca2ce20e415b982d
“MythMega” <unconfigured@null.spigotmc.org>
2022-05-14 19:01:48 +0200

    admin integration

--- mmk8ds/statsviewers/admin.py
+++ mmk8ds/statsviewers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Courses, Driver, Body, Tire, Glider, Build, Gametype, Cup, Track, Courses
 
-# Register your models here.
+admin.site.register(Driver)
+admin.site.register(Body)
+admin.site.register(Tire)
+admin.site.register(Glider)
+admin.site.register(Build)
+admin.site.register(Gametype)
+admin.site.register(Cup)
+admin.site.register(Track)
+admin.site.register(Courses)
