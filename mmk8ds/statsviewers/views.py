954ba2a1f6a4dffb3fc724cfa20ca3014486f0f0
“MythMega” <unconfigured@null.spigotmc.org>
2022-05-14 18:59:41 +0200

    views for all builds, all tracks, index

--- mmk8ds/statsviewers/views.py
+++ mmk8ds/statsviewers/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import Http404, HttpResponse
 from .models import *
 from django.template import loader
@@ -7,4 +6,21 @@ from django.template import loader
 def index(request):
     template = loader.get_template('statsviewers/index.html')
     data = {}
+    return HttpResponse(template.render(data, request))
+
+
+def tracks(request):
+    template = loader.get_template('statsviewers/tracklist.html')
+    alltracks = Cup.objects.all()
+    data = {'itemList':alltracks}
+    return HttpResponse(template.render(data, request))
+
+def builds(request):
+    template = loader.get_template('statsviewers/buildlist.html')
+    allbuilds = Build.objects.all()
+    print('DEBUG')
+    print('DEBUG')
+    print('DEBUG')
+    print(allbuilds)
+    data = {'itemList':allbuilds}
     return HttpResponse(template.render(data, request))
