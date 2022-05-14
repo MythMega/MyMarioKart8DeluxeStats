f1e57815421b386bad8703493f5e2f12904cb303
“MythMega” <unconfigured@null.spigotmc.org>
2022-05-14 19:02:27 +0200

    add app

--- mmk8ds/mmk8ds/settings.py
+++ mmk8ds/mmk8ds/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'statsviewers.apps.StatsviewersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
